15c1c83c49a9caa577c6154c0301be53f5f28768
FumiyaSuzuki <nojino_second@icloud.com>
2024-06-29 17:58:00 +0900

    revalidate=0

--- app/api/posts/all/route.ts
+++ app/api/posts/all/route.ts
@@ -1,7 +1,7 @@
 import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
 
-// export const revalidate = 0;
+export const revalidate = 0;
 
 export async function GET() {
   try {
